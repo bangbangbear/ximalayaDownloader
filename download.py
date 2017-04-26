25027605e5a370dfb0cb40ab9aeddafc89090441
Xuebin Wu <xuebin.wu@avagotech.com>
2017-04-26 14:05:42 -0700

    Rewrite the script in a package fasshion.

--- download.py
+++ download.py
@@ -1,20 +1,33 @@
 # coding=utf-8
 
-import urllib2
+from urllib2 import urlopen, Request
 import json
 import re
 
-# album_url = 'http://www.ximalaya.com/7712455/album/6333174'
-album_url = 'http://www.ximalaya.com/7712455/album/4474664'
-headers = {'User-Agent': 'Safari/537.36'}
-resp = urllib2.urlopen(urllib2.Request(album_url, headers=headers))
-ids = re.search('sound_ids=\"(.*)\"', resp.read()).group(1).split(',')
 
-for ind, f in enumerate(ids):
-    url = 'http://www.ximalaya.com/tracks/{}.json'.format(f)
-    resp = urllib2.urlopen(urllib2.Request(url, headers=headers))
-    data = json.loads(resp.read())
-    output = data['title'] + data['play_path_64'][-4:]
-    print output, data['play_path_64']
-    with open(output, 'wb') as local:
-        local.write(urllib2.urlopen(data['play_path_64']).read())
+class XmlyDownloader(object):
+    def __init__(self):
+        self.headers = {'User-Agent': 'Safari/537.36'}
+
+    def getIDs(self, url):
+        resp = urlopen(Request(url, headers=self.headers))
+        return re.search('sound_ids=\"(.*)\"', resp.read()).group(1).split(',')
+
+    def download_file(self, ID):
+        url = 'http://www.ximalaya.com/tracks/{}.json'.format(ID)
+        resp = urlopen(Request(url, headers=self.headers))
+        data = json.loads(resp.read())
+        output = data['title'] + data['play_path_64'][-4:]
+        print output, data['play_path_64']
+        with open(output, 'wb') as local:
+            local.write(urlopen(data['play_path_64']).read())
+
+    def download_album(self, album_url):
+        for ID in self.getIDs(album_url):
+            self.download_file(ID)
+
+
+if __name__ == '__main__':
+    album_url = 'http://www.ximalaya.com/7712455/album/4474664'
+    xmly = XmlyDownloader()
+    xmly.download_album(album_url)
